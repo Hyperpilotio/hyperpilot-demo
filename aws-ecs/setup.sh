bd497d7a3512032bc513a1a56d3793b7127d7397
Timothy Chen <tnachen@gmail.com>
2016-12-02 07:54:54 +0000

    Fix front end url

--- aws-ecs/setup.sh
+++ aws-ecs/setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+which jq &> /dev/null
+if [ $? -ne 0 ]
+then
+    echo "Please install jq library before setup"
+    exit 1
+fi
+
 cwd=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 

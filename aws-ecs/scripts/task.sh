85d7ad0687f3eba249e481d984ee4b7eab2d056a
swh <swh@hsiang.io>
2016-12-07 16:34:09 +0800

    * Add delete function in aws-ecs/scripts/task.sh to prevent the error of daemon is existing.

--- aws-ecs/scripts/task.sh
+++ aws-ecs/scripts/task.sh
@@ -26,12 +26,47 @@ if [[ "X$AWS_CLOUDFORMATION_STACK" == "X" ]]; then
   AWS_CLOUDFORMATION_STACK="BloxAws"
 fi
 
-echo "Starting cadvisors on each host..."
 # Replace this with daemon ecs scheduler
 ENV_NAME="cadvisor"
 TASK_DEFINITION="cadvisor"
-CADVISOR_DEPLOYMENT_TOKEN="$($DIR/../../blox/deploy/demo-cli/blox-create-environment.py --apigateway --stack $AWS_CLOUDFORMATION_STACK --environment $ENV_NAME --cluster "weave-ecs-demo-cluster" --task-definition $TASK_DEFINITION | jq .deploymentToken | cut -d"\"" -f2)"
-$DIR/../../blox/deploy/demo-cli/blox-create-deployment.py \
+IS_ENVIRONMENT_EXIST="$($DIR/../../blox/deploy/demo-cli/blox-list-environments.py --apigateway --stack BloxAws | jq '.items[] | select(.name == "'"$TASK_DEFINITION"'")')"
+IS_DEPLOYMENT_EXIST=""
+CLUSTER="weave-ecs-demo-cluster"
+
+if [[ "X$IS_ENVIRONMENT_EXIST" == "X" ]]; then
+    # FIXME
+    # create environment --> create deployment with "deploymentToken"
+    # environment exists --> create deployment with "deploymentToken"
+    # Supposedly, environment exists --> previous deployment exists --> create new deployment with "nextToken"
+    # Blox is not production ready. Cli tool doesn't provide this function. Ironely, on the swagger api spec, it define this parameter.
+    # However, the request didn't work even though I followed the spec to send the request.
+    # Workaround. environment exists --> does deployment exist ?
+    #     --treu--> delete environment --> create environment --> create deployment
+    #     --false--> create deployment
+
+    echo "Registering task $TASK_DEFINITION on daemon scheduler.."
+    DEPLOYMENT_TOKEN="$($DIR/../../blox/deploy/demo-cli/blox-create-environment.py --apigateway --stack $AWS_CLOUDFORMATION_STACK --environment $ENV_NAME --cluster $CLUSTER --task-definition $TASK_DEFINITION | jq .deploymentToken | cut -d"\"" -f2)"
+    echo "done."
+fi
+
+IS_DEPLOYMENT_EXIST="$($DIR/../../blox/deploy/demo-cli/blox-list-deployments.py --apigateway --stack $AWS_CLOUDFORMATION_STACK --environment $ENV_NAME | jq '.items[]')"
+
+if [[ "X$IS_DEPLOYMENT_EXIST" == "X" ]]; then
+  echo "None of daemon ($TASK_DEFINITION) is running."
+else
+    $DIR/../../blox/deploy/demo-cli/blox-delete-environment.py \
       --apigateway --stack $AWS_CLOUDFORMATION_STACK \
-      --environment $ENV_NAME \
-      --deployment-token $CADVISOR_DEPLOYMENT_TOKEN
+      --environment $ENV_NAME
+
+    echo "Registering task $TASK_DEFINITION on daemon scheduler.."
+    DEPLOYMENT_TOKEN="$($DIR/../../blox/deploy/demo-cli/blox-create-environment.py --apigateway --stack $AWS_CLOUDFORMATION_STACK --environment $ENV_NAME --cluster $CLUSTER --task-definition $TASK_DEFINITION | jq .deploymentToken | cut -d"\"" -f2)"
+    echo "done."
+fi
+
+echo "Start to deploy daemons on $CLUSTER .."
+DEPLOYMENT_TOKEN="$($DIR/../../blox/deploy/demo-cli/blox-list-environments.py --apigateway --stack $AWS_CLOUDFORMATION_STACK | jq '.items[] | select(.name == "'"$TASK_DEFINITION"'") | .deploymentToken' | cut -d"\"" -f2)"
+$DIR/../../../blox/deploy/demo-cli/blox-create-deployment.py \
+    --apigateway --stack $AWS_CLOUDFORMATION_STACK \
+    --environment $ENV_NAME \
+    --deployment-token $DEPLOYMENT_TOKEN
+echo "done."

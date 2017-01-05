695652c3c5a34d93d64b45c2eb4c867465278786
swh <swh@hsiang.io>
2017-01-06 01:16:22 +0800

    Update to Cassandra 3

--- workloads/cassandra/image/cassandra-stress/run.sh
+++ workloads/cassandra/image/cassandra-stress/run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-
-curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
-
-sudo apt-get update && \
-    sudo apt-get install dsc21=2.1.5-1 && \
-    sudo apt-get install cassandra-tools=2.1.5
-
 /usr/bin/cassandra-stress write n=1000000 cl=one -mode native cql3 -schema keyspace="keyspace1" -log file=~/load_1M_rows.log -sendToDaemon cassandra-service -port 7199 
 

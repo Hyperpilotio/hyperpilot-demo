dfd63bf9c353e2dfc6e0d5f8f2d4804e0ac46099
swh <swh@hsiang.io>
2017-02-15 14:37:36 +0800

    Add new task to test the function of listing locations.

--- workloads/delivery/load-test/locustfile.py
+++ workloads/delivery/load-test/locustfile.py
@@ -3,13 +3,13 @@ from locust import HttpLocust, TaskSet, task
 class StaticTasks(TaskSet):
     @task
     def list_cargos(self):
-       response = self.client.get("/booking/v1/cargos")
-       if response.status_code == 200:
-           response.success()
-       else:
-           response.failure("Got wrong response code {}".format(response.status_code))
+        self.client.get("/booking/v1/cargos")
+
+    @task
+    def list_locations(self):
+        self.client.get("/booking/v1/locations")
 
 class LoggedInUser(HttpLocust):
     task_set = StaticTasks
-    min_wait = 0
-    max_wait = 0
+    min_wait = 5000
+    max_wait = 15000

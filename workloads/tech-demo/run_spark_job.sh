22e7463c8dabfa34e34ccd4397d506f569370bf5
Timothy Chen <tnachen@gmail.com>
2017-03-15 07:16:14 +0000

    Adjust resource settings and spark job script

--- workloads/tech-demo/run_spark_job.sh
+++ workloads/tech-demo/run_spark_job.sh
@@ -1,3 +1,3 @@
 SPARK_MASTER_POD=`kubectl get pods | grep spark-master | cut -d" " -f1`
 
-kubectl exec $SPARK_MASTER_POD -- /bin/bash -c './bin/spark-submit --master=spark://$(hostname -i):6066 --deploy-mode=cluster --class MovieLensALS s3://hyperpilot-jarfiles/movielens-als-assembly-2.11-0.1.jar s3://demo-analysis-datasets/movielens/medium/ s3://demo-analysis-datasets/personalRatings.txt'
+kubectl exec $SPARK_MASTER_POD -- /bin/bash -c './bin/spark-submit --master=spark://$(hostname -i):6066 --deploy-mode=cluster --executor-memory 2g --driver-memory 2g --class MovieLensALS s3://hyperpilot-jarfiles/movielens-als-assembly-2.11-0.1.jar s3://demo-analysis-datasets/movielens/medium/ s3://demo-analysis-datasets/personalRatings.txt'

3096c18f58e2a7a18779cd99297196972299a454
Timothy Chen <tnachen@gmail.com>
2017-03-20 18:02:38 +0000

    Fix snap and add create secret script

---
+++ workloads/tech-demo/create_secret.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DOCKER_REGISTRY_SERVER=https://index.docker.io/v1/
+export DOCKER_USER=hyperpilot
+export DOCKER_EMAIL=tim@hyperpilot.io
+export DOCKER_PASSWORD=hyper123
+
+kubectl create secret docker-registry myregistrykey \
+--docker-server=$DOCKER_REGISTRY_SERVER \
+--docker-username=$DOCKER_USER \
+--docker-password=$DOCKER_PASSWORD \
+--docker-email=$DOCKER_EMAIL

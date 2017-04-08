acbf453710794f49c4102fa228608bd9bb0b7313
Timothy Chen <tnachen@gmail.com>
2017-04-08 23:00:18 +0000

    Fix grafana docker build script

--- workloads/tech-demo/grafana/build.sh
+++ workloads/tech-demo/grafana/build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 export GOOS=linux
 export GOARCH=amd64
-go build . -o config-ds
-docker build -t hyperpilot/tech-demo-grafana .
+go build -o config-ds
+sudo docker build -t hyperpilot/tech-demo-grafana .

7ce828d2145e41d2e779d80452140f0a42349cb4
Timothy Chen <tnachen@gmail.com>
2017-04-08 23:00:49 +0000

    Modify refresh interval to be 5s

--- workloads/tech-demo/deploy-k8s.sh
+++ workloads/tech-demo/deploy-k8s.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+JQ_CHECK=`which jq`
+if ["$JQ_CHECK" == ""]; then
+    echo "Please install jq before running deployment (sudo apt-get install jq, or brew install jq)"
+    exit 1
+fi
+
 curl -XPOST localhost:7777/v1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
 curl -XPOST localhost:7777/v1/files/tech_demo_core_site_xml -F upload=@core-site.xml
-curl -XPOST localhost:7777/v1/deployments --data-binary @deploy-k8s.json
+curl -XPOST localhost:7777/v1/deployments --data-binary @deploy-k8s.json | jq .

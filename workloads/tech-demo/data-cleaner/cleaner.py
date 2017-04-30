ae3e3946279fac73906ed69e43685dae5e2d2ebb
Timothy Chen <tnachen@gmail.com>
2017-04-29 20:04:00 -0700

    Add data cleaner

---
+++ workloads/tech-demo/data-cleaner/cleaner.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from influxdb import InfluxDBClient
+
+# Clean mongo
+def clean_mongo():
+    c = MongoClient(host=['mongodb:27017'])
+    db = c.goddd
+    collections = db.collection_names(include_system_collections=False)
+    for collection in collections:
+        db.drop_collection(collection)
+
+def clean_influx():
+    client = InfluxDBClient(host="influxsrv.hyperpilot", port=8086)
+    databases = client.get_list_database()
+    for row in databases:
+        databaseName = row['name']
+        if databaseName == '_internal':
+            continue
+
+        client.drop_database(databaseName)
+        client.create_database(databaseName)
+
+print("Cleaning mongo")
+clean_mongo()
+print("Cleaning influx")
+clean_influx()

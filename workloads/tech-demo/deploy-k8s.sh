ac570cc6012b7f739677bff09f2775e6cfcd7d40
Timothy Chen <tnachen@gmail.com>
2017-05-17 14:02:10 -0700

    Remove jq check

--- workloads/tech-demo/deploy-k8s.sh
+++ workloads/tech-demo/deploy-k8s.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-JQ_CHECK=`which jq`
-if [ "$JQ_CHECK" == "" ]; then
-    echo "Please install jq before running deployment (sudo apt-get install jq, or brew install jq)"
-    exit 1
-fi
-
 #DEPLOYER_URL="localhost"
 DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
-
 
 curl -XPOST $DEPLOYER_URL:7777/v1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
 curl -XPOST $DEPLOYER_URL:7777/v1/files/tech_demo_core_site_xml -F upload=@core-site.xml

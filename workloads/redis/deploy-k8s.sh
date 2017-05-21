30c97a847ddf200c8eb7f3ecf2b2f2413a1cdb22
Timothy Chen <tnachen@gmail.com>
2017-05-20 23:03:55 -0700

    Update redis to support latest k8s and profile

--- workloads/redis/deploy-k8s.sh
+++ workloads/redis/deploy-k8s.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-#curl -XPOST localhost:7777/v1/files/nodejs_load_test -F upload=@load-test/locustfile.py
-curl -XPOST localhost:7777/v1/deployments --data-binary @deploy-k8s.json
+
+#DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
+DEPLOYER_URL="localhost"
+
+curl -XPOST $DEPLOYER_URL:7777/v1/deployments --data-binary @deploy-k8s.json
+
+echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"

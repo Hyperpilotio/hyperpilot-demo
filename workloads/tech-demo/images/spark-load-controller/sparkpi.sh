38c8040a24b955bf7187cf5735f484f91098c9bd
Timothy Chen <tnachen@gmail.com>
2017-06-21 16:52:52 -0700

    Select multiple jobs for spark demo

---
+++ workloads/tech-demo/images/spark-load-controller/sparkpi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SPARK_MASTER=spark-master.default:6066
+
+./bin/spark-submit --master=spark://$SPARK_MASTER --deploy-mode=cluster --executor-cores 1 --total-executor-cores 2 --executor-memory 1g --driver-memory 2g --class org.apache.spark.examples.SparkPi file:///usr/spark-2.1.0/examples/jars/spark-examples_2.11-2.1.0.jar

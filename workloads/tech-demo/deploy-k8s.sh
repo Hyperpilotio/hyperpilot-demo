4fb80094edeb56babc8558d3fae6a8ff18e5dfb3
Timothy Chen <tnachen@gmail.com>
2017-06-22 12:16:40 -0700

    Update deploy-k8s.sh to provide user id

--- workloads/tech-demo/deploy-k8s.sh
+++ workloads/tech-demo/deploy-k8s.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 1 ]
+then
+    echo "Usage: deploy-k8s.sh <userId>"
+    exit 1
+fi
+
 #DEPLOYER_URL="localhost"
 DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
 
-curl -XPOST $DEPLOYER_URL:7777/v1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
-curl -XPOST $DEPLOYER_URL:7777/v1/files/tech_demo_core_site_xml -F upload=@core-site.xml
-curl -s -XPOST $DEPLOYER_URL:7777/v1/deployments --data-binary @deploy-k8s.json
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_core_site_xml -F upload=@core-site.xml
+curl -s -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @deploy-k8s.json
 
 echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"

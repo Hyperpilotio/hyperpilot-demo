e3b63925e2a25384e300685ef3588e6fd125b713
alan.yang <jpra1113@gmail.com>
2017-06-22 10:03:24 +0800

    Rename

--- workloads/analysis-base.sh
+++ workloads/analysis-base.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
-curl -XPOST $DEPLOYER_URL:7777/v1/templates/redis-base --data-binary @base.json
+curl -XPOST $DEPLOYER_URL:7777/v1/templates/analysis-base --data-binary @analysis-base.json

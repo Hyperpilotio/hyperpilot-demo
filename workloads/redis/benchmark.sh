e5722b53bd4bc734736407ec145251a5b5e3c7fe
alan.yang <jpra1113@gmail.com>
2017-06-23 14:15:14 +0800

    Add share profiler url

--- workloads/redis/benchmark.sh
+++ workloads/redis/benchmark.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-curl -XPOST -H "Content-Type: application/json" localhost:7779/benchmarks/redis -d "{\"deploymentId\":\"$1\", \"startingIntensity\":25, \"step\": 25}"
+
+#PROFILER_URL="internal-profiler-25087965.us-east-1.elb.amazonaws.com"
+PROFILER_URL="localhost"
+
+curl -XPOST -H "Content-Type: application/json" $PROFILER_URL:7779/benchmarks/redis -d "{\"startingIntensity\":25, \"step\": 25}"
+
+echo "Please check progress of your deployment at http://$PROFILER_URL:7779/ui"
+
+

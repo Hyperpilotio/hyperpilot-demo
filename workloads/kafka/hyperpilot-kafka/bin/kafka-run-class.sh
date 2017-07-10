c9324b80d9fd99e4fc9e1be666f773a9fc22f34e
Che-Yuan Liang <cheyuanl@andrew.cmu.edu>
2017-07-10 15:06:14 -0700

    Changed kafka-serve to localhost

--- workloads/kafka/hyperpilot-kafka/bin/kafka-run-class.sh
+++ workloads/kafka/hyperpilot-kafka/bin/kafka-run-class.sh
@@ -142,7 +142,7 @@ shopt -u nullglob
 
 # JMX settings
 if [ -z "$KAFKA_JMX_OPTS" ]; then
-  KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=kafka-serve:9092 -Djava.net.preferIPv4Stack=true"
+  KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost:9092 -Djava.net.preferIPv4Stack=true"
 fi
 
 # JMX port to use

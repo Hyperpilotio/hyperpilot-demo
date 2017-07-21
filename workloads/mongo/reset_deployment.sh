976cb876d17af5742d1153645307090bd81c5603
Timothy Chen <tnachen@gmail.com>
2017-07-21 10:49:53 -0700

    Add user id to reset

--- workloads/mongo/reset_deployment.sh
+++ workloads/mongo/reset_deployment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DEPLOYMENT_NAME=$1
+DEPLOYMENT_NAME=$2
 
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
     DEPLOYMENT_NAME=`kubectl get nodes --show-labels | grep deployment | tr "," "\n" | grep deployment | head -n 1 | cut -d"=" -f2`
 
     if [ -z "$DEPLOYMENT_NAME" ]; then
@@ -12,9 +12,9 @@ if [ -z "$1" ]; then
 
 fi
 
-DEPLOYER_URL="localhost"
-#DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
+#DEPLOYER_URL="localhost"
+DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
 
 echo "Resetting demo with deployment $DEPLOYMENT_NAME"
 
-curl -XPUT $DEPLOYER_URL:7777/v1/deployments/$DEPLOYMENT_NAME --data-binary @deploy-k8s.json
+curl -XPUT $DEPLOYER_URL:7777/v1/users/$1/deployments/$DEPLOYMENT_NAME --data-binary @deploy-k8s.json

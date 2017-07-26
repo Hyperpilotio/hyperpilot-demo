2ee680b44d35c9c1c5ee778e24cb99b5a38cd013
William Chen <chen.biwei@gmail.com>
2017-07-26 16:34:22 +0800

    1. Add networkPerformance to networkConfig 2. move EBS related parameters into storageConfig 3. merge instanceFamily with category 4. add ecu to cpuConfig

--- workloads/sizing-demo/ec2_instance_type_crawler.py
+++ workloads/sizing-demo/ec2_instance_type_crawler.py
@@ -35,7 +35,7 @@ reservedPricing = {}
 cpuCredits = []
 bandwidth = []
 clusterNetworking = ["r4", "x1", "m4", "c4", "c3", "i2", "cr1", "hs1", "p2", "g3", "d2"]
-def getNetworkBandwidth(instanceType):
+def getEBSConfig(instanceType):
     if len(bandwidth) <= 0:
         mp = soup(urllib2.urlopen("http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-ec2-config.html"), "html.parser")
         table = mp.find(id="w144aac23c29c25b9c15")
@@ -120,15 +120,12 @@ def compose(region):
                          x["attributes"]["tenancy"] == "Shared", products.values())[0]
         result = ObjDict()
         result.name = insType
-        result.instanceFamily = insType.split(".")[0]
-        if "instanceFamily" not in product["attributes"]:
-          continue
-        result.category = product["attributes"]["instanceFamily"]
-
+        result.instanceFamily = product["attributes"].get("instanceFamily", "")
         result.hourlyCost = findPrice(insType)
 
         result.cpuConfig = ObjDict()
-        result.cpuConfig.vCPU = int(product["attributes"]["vcpu"])
+        result.cpuConfig.vCPU = int(product["attributes"].get("vcpu", 0))
+        result.cpuConfig.ecu = product["attributes"].get("ecu", "0")
         result.cpuConfig.cpuCredits = 0
         credits = getCpuCredits(insType)
         if credits is None:
@@ -142,7 +139,7 @@ def compose(region):
             credit.vCPUs = int(credits[KEY_CPU_CREDITS_VCPUS])
             result.cpuConfig.cpuCredits = credit
 
-        result.cpuConfig.cpuType = product["attributes"]["physicalProcessor"]
+        result.cpuConfig.cpuType = product["attributes"].get("physicalProcessor", "")
         clockSpeed = re.sub(' +', ' ', product["attributes"].get("clockSpeed", "0")).split(" ")
         clockSpeedValueIndex = 0
         try:
@@ -155,20 +152,8 @@ def compose(region):
         result.memoryConfig.size = makeValueWithUnit(float(product["attributes"].get("memory", "0").split(" ")[0].replace(",", "")), "GiB")
 
         result.networkConfig = ObjDict()
-        result.networkConfig.performance = product["attributes"]["networkPerformance"]
-
-        netItem = getNetworkBandwidth(insType)
-        if netItem is None:
-            result.networkConfig.bandwidth = 0
-        else:
-            networkBandwidth = ObjDict()
-            networkBandwidth.EBSOptimized = netItem[KEY_NETWORK_BANDWIDTH_DEFAULT_EBS_OPTIMIZE]
-            networkBandwidth.max = makeValueWithUnit(int(netItem[KEY_NETWORK_BANDWIDTH_MAX].replace(",","")), "Mbps")
-            networkBandwidth.expectedThroughput = makeValueWithUnit(float(netItem[KEY_NETWORK_BANDWIDTH_EXPECTED_THROUGHPUT].replace(",", "")), "MB/s")
-            networkBandwidth.maxIOPS = makeValueWithUnit(int(netItem[KEY_NETWORK_BANDWIDTH_MAX_IOPS].replace(",", "")), "16KB I/O size")
-            networkBandwidth.clusterNetworking = True if insType.split(".")[0] in clusterNetworking else False
-            result.networkConfig = networkBandwidth
-        result.networkConfig.enhancedNetworking = False
+        result.networkConfig.performance = product["attributes"].get("networkPerformance", "")
+        result.networkConfig.clusterNetworking = True if insType.split(".")[0] in clusterNetworking else False
 
         result.storageConfig = ObjDict()
         storage = product["attributes"]["storage"]
@@ -181,7 +166,15 @@ def compose(region):
             result.storageConfig.devices = int(parse[0])
             result.storageConfig.size = makeValueWithUnit(int(parse[2].replace(",", "")), "GB")
             result.storageConfig.storageType = "HDD" if len(parse) < 4 else parse[3]
-        result.storageConfig.bandwidth = makeValueWithUnit(int(product["attributes"].get("dedicatedEbsThroughput", "0").split(" ")[0]), "Mbps")
+        # result.storageConfig.bandwidth = makeValueWithUnit(int(product["attributes"].get("dedicatedEbsThroughput", "0").split(" ")[0]), "Mbps")
+
+        ebsItem = getEBSConfig(insType)
+        if ebsItem is not None:
+            result.storageConfig.EBSOptimized = ebsItem[KEY_NETWORK_BANDWIDTH_DEFAULT_EBS_OPTIMIZE]
+            result.storageConfig.bandwidth = makeValueWithUnit(int(ebsItem[KEY_NETWORK_BANDWIDTH_MAX].replace(",","")), "Mbps")
+            result.storageConfig.expectedThroughput = makeValueWithUnit(float(ebsItem[KEY_NETWORK_BANDWIDTH_EXPECTED_THROUGHPUT].replace(",", "")), "MB/s")
+            result.storageConfig.maxIOPS = makeValueWithUnit(int(ebsItem[KEY_NETWORK_BANDWIDTH_MAX_IOPS].replace(",", "")), "16KB I/O size")
+
         ec2Instances.data.append(result)
     return ec2Instances
 

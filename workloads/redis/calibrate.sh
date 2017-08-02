59d4338f7b4459d209abbea52e243dadc3b18aac
swhsiang <swh@hsiang.io>
2017-08-02 15:30:45 -0700

    Move comands from separated script files into a Makfile

--- workloads/redis/calibrate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -XPOST -H "Content-Type: application/json" http://internal-profiler-25087965.us-east-1.elb.amazonaws.com:7779/calibrate/redis -d "{\"deploymentId\":\"$1\"}"

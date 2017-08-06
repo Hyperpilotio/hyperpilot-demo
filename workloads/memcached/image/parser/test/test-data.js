651a25368133c6048f233f7a49f12b6059da47c9
swhsiang <swh@hsiang.io>
2017-08-06 15:55:39 -0700

    Refactor parser

--- workloads/memcached/image/parser/test/test-data.js
+++ workloads/memcached/image/parser/test/test-data.js
@@ -2,19 +2,19 @@ const PARSER_DATASET = {
     'memcached': [
         {
             'input': `#type       avg     std     min     5th    10th    90th    95th    99th
-read       78.4   116.7    34.0    45.0    51.5   115.7   144.5   204.3
+read    108463.8 34243.1 92709.1 93636.2 94563.3 111855.9 145236.8 306672.7
 update      0.0     0.0     0.0     0.0     0.0     0.0     0.0     0.0
 op_q        1.0     0.0     1.0     1.0     1.0     1.1     1.1     1.1
 
-Total QPS = 12664.0 (63320 / 5.0s)
+Total QPS = 9.2 (46 / 5.0s)
 
 Misses = 0 (0.0%)
 Skipped TXs = 0 (0.0%)
 
-RX   15700040 bytes :    3.0 MB/s
-TX    2279556 bytes :    0.4 MB/s`,
+RX      71362 bytes :    0.0 MB/s
+TX       1692 bytes :    0.0 MB/s`,
             'expect': {
-                'TotalQps': 12664.0
+                'TotalQps': 9.2
             }
         }
     ]

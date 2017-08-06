c1847c669e08059b3f1997416c766090e6bc9925
swhsiang <swh@hsiang.io>
2017-08-06 15:55:39 -0700

    Refactor parser

--- workloads/memcached/image/parser/parser.js
+++ workloads/memcached/image/parser/parser.js
@@ -4,61 +4,68 @@ class Parser {
       false : options.verbose;
   }
   processLines(lines = []) {
-    if (lines.length != 12) {
-      console.error(`Warn: the format of input is wrong\nResult: ${lines}`);
-      return {};
+    // Remove empty string from array
+    const len = lines.length;
+    for (let i = 0; i < len; i++) {
+      lines[i] !== '' && lines.push(lines[i]);
     }
+    lines.splice(0, len);
+
     const benchmarkObj = {};
 
-    try {
-      const operationList = ['read', 'update', 'op_q'];
-      // microsecond
-      const typeList = ['avg', 'std', 'min', '5th', '10th', '90th', '95th', '99th'];
+    const operationList = ['read', 'update', 'op_q'];
+    // microsecond
+    const typeList = ['avg', 'std', 'min', '5th', '10th', '90th', '95th', '99th'];
 
-      const regex = /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g;
-      for (let i = 0; i < operationList.length; i++) {
-        const str = lines[i + 1];
-        let m = {};
-        let valueArr = [];
-        while ((m = regex.exec(str)) !== null) {
-          if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-          }
-          valueArr.push(parseFloat(m[0]));
-        }
+    let index = 0;
+    const firstLine = '#type       avg     std     min     5th    10th    90th    95th    99th'
+    for (index = 0; index < lines.length; index++) {
+      if (lines[index] === firstLine) {
+        index++;
+        break;
+      }
+    }
 
-        for (let j = 0; j < typeList.length; j++) {
-          benchmarkObj[`${operationList[i]}_${typeList[j]}`] = valueArr[j];
+    const regex = /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g;
+    for (let i = 0; i < operationList.length; i++) {
+      const str = lines[index + i];
+      let m = {};
+      let valueArr = [];
+      while ((m = regex.exec(str)) !== null) {
+        if (m.index === regex.lastIndex) {
+          regex.lastIndex++;
         }
+        valueArr.push(parseFloat(m[0]));
       }
 
-
-      let arr = [];
-
-      arr = (/(\d+.\d+)\s+\((\d+\s+\/\s+\d+.\d+s)\)/g).exec(lines[5]);
-      benchmarkObj['TotalQps'] = parseFloat(arr[1]);
-      benchmarkObj['TotalQpsFraction'] = arr[2];
-
-      arr = (/(\d+)\ \((\d+.\d+)/g).exec(lines[7])
-      benchmarkObj['Misses'] = parseInt(arr[1]);
-      benchmarkObj['MissesPercentage'] = parseFloat(arr[2]);
-
-      arr = (/(\d+)\ \((\d+.\d+)/g).exec(lines[8])
-      benchmarkObj['SkippedTXs'] = parseInt(arr[1]);
-      benchmarkObj['SkippedTXsPercentage'] = parseFloat(arr[2]);
-
-      arr = (/(\d+\ bytes)\s+:\s+(\d+.\d+\ [TKMG]B\/.)/g).exec(lines[10]);
-      benchmarkObj['RXBytes'] = arr[1];
-      benchmarkObj['RX_avg'] = arr[2];
-
-      arr = (/(\d+\ bytes)\s+:\s+(\d+.\d+\ [TKMG]B\/.)/g).exec(lines[11]);
-      benchmarkObj['TXBytes'] = arr[1];
-      benchmarkObj['TX_avg'] = arr[2];
-
+      for (let j = 0; j < typeList.length; j++) {
+        benchmarkObj[`${operationList[i]}_${typeList[j]}`] = valueArr[j];
+      }
     }
-    catch (err) {
-      console.error(err)
-    }
+
+    index = index + operationList.length;
+
+    let arr = [];
+
+    arr = (/(\d+.\d+)\s+\((\d+\s+\/\s+\d+.\d+s)\)/g).exec(lines[index++]);
+    benchmarkObj['TotalQps'] = parseFloat(arr[1]);
+    benchmarkObj['TotalQpsFraction'] = arr[2];
+
+    arr = (/(\d+)\ \((\d+.\d+)/g).exec(lines[index++])
+    benchmarkObj['Misses'] = parseInt(arr[1]);
+    benchmarkObj['MissesPercentage'] = parseFloat(arr[2]);
+
+    arr = (/(\d+)\ \((\d+.\d+)/g).exec(lines[index++])
+    benchmarkObj['SkippedTXs'] = parseInt(arr[1]);
+    benchmarkObj['SkippedTXsPercentage'] = parseFloat(arr[2]);
+
+    arr = (/(\d+\ bytes)\s+:\s+(\d+.\d+\ [TKMG]B\/.)/g).exec(lines[index++]);
+    benchmarkObj['RXBytes'] = arr[1];
+    benchmarkObj['RX_avg'] = arr[2];
+
+    arr = (/(\d+\ bytes)\s+:\s+(\d+.\d+\ [TKMG]B\/.)/g).exec(lines[index++]);
+    benchmarkObj['TXBytes'] = arr[1];
+    benchmarkObj['TX_avg'] = arr[2];
 
     return benchmarkObj;
   }

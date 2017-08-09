c1e6e271a82e7b7d05cd34fe60113ad0524c0e84
William Chen <chen.biwei@gmail.com>
2017-08-10 02:23:42 +0800

    finish lucene workload

--- workloads/lucene/images/parser/parser.js
+++ workloads/lucene/images/parser/parser.js
@@ -1,39 +1,43 @@
+var xml2js = require('xml2js');
 
 class Parser {
-  constructor(options) {
-    this.isVerbose = (options === undefined || options.verbose === undefined) ?
-                      false : options.verbose;
-  }
-  processLines(lines = []) {
-    let benchmarkObj = {};
-    let grabValue = false;
-    let xmlString = "";
-    let m = []
-    // const regex = /<metric[^>]* unit="(.*?)"[^>]*>(.*?)<\/metric>/;
-    for (let i = 0; i < lines.length; i++) {
-      if (lines[i].indexOf("<benchResults>") > -1) {
-          grabValue = true;
-      }
-      if (grabValue) {
-          xmlString = xmlString.concat(lines[i]);
-      }
-      if (lines[i].indexOf("</benchResults>") > -1) {
-          grabValue = false
-      }
+    constructor(options) {
+        this.isVerbose = (options === undefined || options.verbose === undefined) ?
+                        false : options.verbose;
     }
-    if (xmlString) {
-        require('xml2js').parseString(xmlString, (err, result) => {
-            if (err) {
-                throw err
-            } else {
-                let summary = result.benchResults.benchSummary[0].metric[0]
-                benchmarkObj[summary.$["unit"]] = summary["_"]
+
+    processLines(lines = []) {
+        let benchmarkObj = {};
+        let grabValue = false;
+        let xmlString = "";
+        let m = []
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].indexOf("<benchResults>") > -1) {
+                grabValue = true;
             }
-        })
+            if (grabValue) {
+                xmlString = xmlString.concat(lines[i]);
+            }
+            if (lines[i].indexOf("</benchResults>") > -1) {
+                grabValue = false;
+            }
+        }
+        if (xmlString) {
+            let parser = new xml2js.Parser();
+            parser.parseString(xmlString, (err, result) => {
+                if (err) {
+                    throw err;
+                    return null;
+                } else {
+                    let summary = result.benchResults.benchSummary[0].metric[0];
+                    benchmarkObj[summary.$["unit"]] = parseFloat(summary["_"]);
+                }
+            });
+            // wait until done parsing
+            while(benchmarkObj.length <= 0) {};
+            return benchmarkObj;
+        }
     }
-    console.log(benchmarkObj)
-    return benchmarkObj;
-  }
 }
 
 module.exports = Parser;

9dc9b1465bd153f970846768a68ca8e68b6b74e5
Timothy Chen <tnachen@gmail.com>
2017-08-18 02:10:08 -0700

    All mongo calibration

--- workloads/kafka/calibrate.sh
+++ workloads/kafka/calibrate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -XPOST -H "Content-Type: application/json" http://internal-profiler-25087965.us-east-1.elb.amazonaws.com:7779/calibrate/kafka -d "{\"deploymentId\":\"$1\"}"
+curl -XPOST -H "Content-Type: application/json" http://localhost:7779/calibrate/kafka
---
+++ workloads/mongo/calibrate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -XPOST -H "Content-Type: application/json" http://localhost:7779/calibrate/mongo

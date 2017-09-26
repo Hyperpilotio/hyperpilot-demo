a987310d6980569b3d5aa9748775c80cbdf804c7
William Chen <chen.biwei@gmail.com>
2017-09-26 14:02:43 +0800

    split snap-k8s-kafka into deploy-k8s-kafka-influx.json

--- workloads/tech-demo/deploy-k8s.sh
+++ workloads/tech-demo/deploy-k8s.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-    echo "Usage: deploy-k8s.sh <userId>"
+    echo "Usage: deploy-k8s.sh <userId> <mode:optional>"
     exit 1
 fi
 
 DEPLOYER_URL="localhost"
 #DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
 
+if [ "-$2" = "-" ]; then
+  MODE=""
+else
+  MODE="-$2"
+fi
+
+FILE_NAME="deploy-k8s$MODE.json"
+
 curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
 curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_core_site_xml -F upload=@core-site.xml
 curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_kafka_influxdb -F upload=@images/kafka-influxdb/config.yaml
-curl -s -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @deploy-k8s.json
+curl -s -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @$FILE_NAME
 
 echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"

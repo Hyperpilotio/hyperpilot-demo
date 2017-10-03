cd67c76a3e3b68d8b8b9ee5eecfbfa06781ac435
Timothy Chen <tnachen@gmail.com>
2017-10-03 14:25:58 -0700

    Fix upload script to upload all templates

--- workloads/common/upload.sh
+++ workloads/common/upload.sh
@@ -1,3 +1,3 @@
-curl -XPOST $1:7777/v1/templates/analysis-base --data-binary @templates/analysis-base.json
-curl -XPOST $1:7777/v1/templates/memory-optimize --data-binary @templates/memory-optimize-with-benchmark-controller.json
-curl -XPOST $1:7777/v1/templates/analysis-base-3-nodes --data-binary @templates/analysis-base-3-nodes.json
+#!/bin/bash
+
+ls templates/*.json | cut -d"/" -f2 | cut -d"." -f1 | xargs -I{} curl -XPOST $1:7777/v1/templates/{} --data-binary @templates/{}.json

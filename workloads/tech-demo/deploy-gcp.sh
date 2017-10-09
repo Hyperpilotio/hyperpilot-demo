72a93114a05b10916b3917b7c87b75091fbc0bbe
Timothy Chen <tnachen@gmail.com>
2017-10-09 15:17:27 -0700

    Add deploy gcp json

---
+++ workloads/tech-demo/deploy-gcp.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "$#" -lt 1 ]
+then
+    echo "Usage: deploy-gcp.sh <userId> <mode:optional>"
+    exit 1
+fi
+
+DEPLOYER_URL="localhost"
+#DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
+
+if [ "-$2" = "-" ]; then
+  MODE=""
+else
+  MODE="-$2"
+fi
+
+FILE_NAME="deploy-gcp$MODE.json"
+
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_load_test -F upload=@load-test/locustfile.py
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_core_site_gcs_xml -F upload=@core-site-gcs.xml
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/tech_demo_kafka_influxdb -F upload=@images/kafka-influxdb/config.yaml
+curl -s -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @$FILE_NAME
+
+echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"

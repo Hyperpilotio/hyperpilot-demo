d41c7e02eafb77a0131563a5a79804c8535e7bf2
Timothy Chen <tnachen@gmail.com>
2017-10-25 10:09:30 -0700

    Add gcp service account file to incluster

--- workloads/in-cluster/deploy-gcp.sh
+++ workloads/in-cluster/deploy-gcp.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
     echo "Usage: deploy-gcp.sh <userId>"
     exit 1
 fi
 
-SERVICE_ACCOUNT_FILE="gcpServiceAccount.json"
-if [ -f "$SERVICE_ACCOUNT_FILE" ]
-then
-	echo "$SERVICE_ACCOUNT_FILE found."
-else
-	echo "$SERVICE_ACCOUNT_FILE not found."
-    exit 1
-fi
-
-#DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
 DEPLOYER_URL="localhost"
+#DEPLOYER_URL="internal-deployer-605796188.us-east-1.elb.amazonaws.com"
 
-curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/gcpServiceAccount_json -F upload=@$SERVICE_ACCOUNT_FILE
-curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @deploy-gcp.json
+FILE_NAME="deploy-gcp.json"
+
+curl -XPOST $DEPLOYER_URL:7777/v1/users/$1/files/gcpServiceAccount_json -F upload=@${HOME}/gcpServiceAccount.json
+curl -s -XPOST $DEPLOYER_URL:7777/v1/users/$1/deployments --data-binary @$FILE_NAME
 
 echo "Please check progress of your deployment at http://$DEPLOYER_URL:7777/ui"

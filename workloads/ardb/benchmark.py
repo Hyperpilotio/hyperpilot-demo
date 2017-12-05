2db68700c8fa79cdbed07cea9cdb37e9eb3d0ffe
alan.yang <jpra1113@gmail.com>
2017-12-05 13:52:55 +0800

    Use redis-benchmark to test and get results

--- workloads/ardb/benchmark.py
+++ workloads/ardb/benchmark.py
@@ -6,6 +6,8 @@ import time
 from collections import deque
 import threading
 from influxdb import InfluxDBClient
+import subprocess
+from parse import compile
 
 q = deque()
 
@@ -19,14 +21,13 @@ class Deployer(object):
                                 deployment_id, "services", service, "url"])
         return requests.get(service_url).text
 
-    def get_benchmark_service_url(self, deployment_id):
-        benchmark_controller_base_url = self.get_service_url(
-            deployment_id, "benchmark-controller")
-        return "http://%s" % "/".join(
-            [benchmark_controller_base_url, "api", "benchmarks"])
-
     def get_ardb_serve_url(self, deployment_id):
-        return self.get_service_url(deployment_id, "ardb-serve")
+        ardb_serve_url = self.get_service_url(deployment_id, "ardb-serve")
+        return ardb_serve_url
+
+    def get_influxsrv_url(self, deployment_id):
+        influxsrv_url = self.get_service_url(deployment_id, "influxsrv")
+        return influxsrv_url
 
 
 class BenchmarkAgent(object):
@@ -42,56 +43,47 @@ class BenchmarkController(object):
     def __init__(self, deployer_client, deployment_id, load_test_config):
         self.deployer_client = deployer_client
         self.load_test_config = load_test_config
-        self.benchmark_service_url = deployer_client.get_benchmark_service_url(
-            deployment_id)
         self.ardb_serve_url = deployer_client.get_ardb_serve_url(deployment_id)
-
         ardb_host, ardb_port = self.ardb_serve_url.split(":")
         self.load_test_config["loadTest"]["args"].extend(
-            ["-h", ardb_host, "-p", int(ardb_port)])
+            ["-h", ardb_host, "-p", ardb_port])
 
     def run_benchmark(self):
-        stage_id = "ardb-%s" % uuid.uuid4()
-        self.load_test_config["stageId"] = stage_id
-        resp = requests.post(self.benchmark_service_url,
-                             json=self.load_test_config)
-        if resp.status_code > 300:
-            print("Unable to send benchmark request to controller")
-        else:
-            self.get_benchmark_result(stage_id)
+        args = "%s %s" % (self.load_test_config["loadTest"]["path"],
+                          " ".join(self.load_test_config["loadTest"]["args"]))
+        p = subprocess.Popen(
+            args,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+        benchmark_result = {}
+        for line in p.stdout.readlines():
+            print(line)
+            fields = compile('"{}","{}"').parse(line)
+            if fields:
+                benchmark_result[fields[0]] = float(fields[1])
 
-    def get_benchmark_result(self, stage_id):
-        stage_url = "%s/%s" % (self.benchmark_service_url, stage_id)
-        result = self.wait_until_benchmark_status_success(stage_url, 30 * 60)
-        if not result:
-            print("Unable to get benchmark results")
+        q.append(benchmark_result)
         self.run_benchmark()
-
-    def wait_until_benchmark_status_success(self, stage_url, timeout, stepTime=10):
-        mustend = time.time() + timeout
-        while time.time() < mustend:
-            results = requests.get(stage_url).json()
-            if results.get("error", "") != "":
-                print("Benchmark failed with error: " + results["error"])
-            elif results["status"] == "running":
-                time.sleep(stepTime)
-            else:
-                q.append(results["results"][0]["results"])
-                return True
-
-        return False
 
 
 class Influx(object):
     def __init__(self, host, port, dbname):
-        self.client = InfluxDBClient(host, port, "root", "root", dbname)
+        self.client = InfluxDBClient(
+            host, port, "root", "root", dbname)
 
     def write_data(self, data, measurement):
+        fields = {}
+        for k in data:
+            if type(data[k]) == int:
+                fields[k] = float(data[k])
+            else:
+                fields[k] = data[k]
         json_body = [
             {
                 "measurement": measurement,
                 "tags": {},
-                "fields": data
+                "fields": fields
             }
         ]
         self.client.write_points(json_body)
@@ -118,21 +110,24 @@ if __name__ == '__main__':
     config_file = "./loadtest_config.json"
     deployer_url = "localhost"
     deployment_id = "ardb-de9e919f"
-    interval = None
     with open(config_file, 'r') as f:
         j = json.load(f)
         lock = threading.Lock()
         interval = j["schedule"]["interval"]
+        measurement = j["measurement"]
+        database = j["database"]
         for i in range(j["schedule"]["workerCount"]):
             BenchmarkWorker(config_file,
                             deployer_url,
                             deployment_id,
                             lock,
-                            'thread-' + str(i)).start()
+                            "benchmarkWorker" + str(i)).start()
 
-    influx_client = Influx("localhost", 8086, "ardb")
-    while True:
-        if q:
-            p = q.popleft()
-            influx_client.write_data(p, "ardb_benchmarks")
-        time.sleep(int(interval))
+        influxsrv_url = Deployer(deployer_url).get_influxsrv_url(deployment_id)
+        db_host = influxsrv_url.split(":")[0]
+        influx_client = Influx(db_host, 8086, database)
+        while True:
+            if q:
+                p = q.popleft()
+                influx_client.write_data(p, measurement)
+            time.sleep(int(interval))
